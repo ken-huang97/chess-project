610825897a0cfce7739cfd8f21b624b6a4b5ed92
ken-huang97 <ktkhuang@hotmail.com>
2016-07-25 18:05:34 -0400

    Add files via upload

---
+++ board.h
@@ -0,0 +1,35 @@
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+class Piece;
+class Moving;
+
+class Board {
+                Piece* piece_array[][];
+                std::vector<Piece*> black_pieces;
+                std::vector<Piece*> white_pieces;
+                std::vector<Moving> moves_made;
+                std::vector<Piece*> destroy;
+                std::vector<Moving> possible_moves;
+				
+				Controller * controller1;
+				Controller * controller2;
+				
+				void calc_all_valid_moves(int player);
+        public:
+                Board();
+                ~Board();
+                void setBoard();
+                void place_piece();
+                void reset();
+                void draw();
+				std::vector<Moving> get_valid_moves(int player);
+				Piece * get_piece(Coord posn);
+				Piece * get_king(int player);
+				void call_move(int player);
+				void call_move(Moving move, int player);
+				void call_move(Moving move, int name_value, int player);
+				void add_players(int p1, int p2);
+}
+
+#endif
