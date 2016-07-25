fcd0b516b68d1444f0dcdd3f00f52db9eded88fd
hakhawaj <hakhawaj@uwaterloo.ca>
2016-07-25 19:58:49 -0400

    Update rook.h

--- rook.h
+++ rook.h
@@ -1,6 +1,8 @@
 #ifndef __ROOK_H__
 #define __ROOK_H__
 #include "piece.h"
+#include "coord.h"
+#include <vector>
 
 struct Coord;
 
@@ -8,5 +10,5 @@ class Rook : public Piece {
 	bool first_move;
 public:
 	std::vector<Coord> calculateValidMoves();
-	bool isFirstMove();
+	bool get_first_move();
 }
