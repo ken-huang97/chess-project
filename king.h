e55c48e5fcf99766f7abd71350edfdbe07f9cde8
ken-huang97 <ktkhuang@hotmail.com>
2016-07-25 19:28:28 -0400

    Update king.h

--- king.h
+++ king.h
@@ -7,6 +7,12 @@ class King::public Piece {
 	const int num_possible_moves = 10;
 	Coord possible_moves[num_possible_moves];
 	bool first_move;
+public:
+	void first_move_off();
+	void first_move_on();
+	bool get_first_move();
+	std::vector<Coord> calc_valid_moves();
+	std::vector<bool> get_is_castle();
 }
 
 #endif
