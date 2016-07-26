a5ae8539f5f2946ebcf89df7874ea68af492199f
TheRealKevinGitHub <hbhappyboy3@gmail.com>
2016-07-25 21:36:30 -0400

    Update pawn.cc

--- pawn.cc
+++ pawn.cc
@@ -35,42 +35,44 @@ std::vector<Coord> Pawn::calc_valid_moves(){
 	//valid is a vector that contains valid place to move to if king is not checked
 	std::vector<Coord> valid;
 	//Makes sure move result doesn't go off the board
-	if(!(possible_move[i].x + posn.x > 7 || possible_move[i].x + posn.x < 0 || possible_move[i].y + posn.y > 7 || possible_move[i].y + posn.y < 0)){
-		//Check to see if the pawn can move forward
-		//The is_pinned is for making sure that the pawn moving won't cause the king to be checked
-		if(board->get_piece(Coord(possible_move[0].x + posn.x, possible_move[0].y + posn.y))==nullptr && (is_pinned()==-1 || is_pinned()==0)){
-			valid.push_back(Coord(possible_move[i].x + posn.x, possible_move[i].y + posn.y));
-		}
+	for(int i=0; i<4; i++){
+		if(!(possible_move[i].x + posn.x > 7 || possible_move[i].x + posn.x < 0 || possible_move[i].y + posn.y > 7 || possible_move[i].y + posn.y < 0)){
+			//Check to see if the pawn can move forward
+			//The is_pinned is for making sure that the pawn moving won't cause the king to be checked
+			if(i==0 && board->get_piece(Coord(possible_move[0].x + posn.x, possible_move[0].y + posn.y))==nullptr && (is_pinned()==-1 || is_pinned()==0)){
+				valid.push_back(Coord(possible_move[i].x + posn.x, possible_move[i].y + posn.y));
+			}
 
-		//Check to see if the pawn can move sideways to capture a piece
-		else if(board->get_piece(Coord(possible_move[1].x + posn.x, possible_move[1].y + posn.y))!=nullptr){
-			if(board->get_piece(Coord(possible_move[1].x + posn.x, possible_move[1].y + posn.y))->is_black() != this->black && (is_pinned()==-1 || is_pinned()==1)){
-				valid.push_back(Coord(possible_move[1].x + posn.x, possible_move[1].y + posn.y));
+			//Check to see if the pawn can move sideways to capture a piece
+			else if(i==1 && board->get_piece(Coord(possible_move[1].x + posn.x, possible_move[1].y + posn.y))!=nullptr){
+				if(board->get_piece(Coord(possible_move[1].x + posn.x, possible_move[1].y + posn.y))->is_black() != this->black && (is_pinned()==-1 || is_pinned()==1)){
+					valid.push_back(Coord(possible_move[1].x + posn.x, possible_move[1].y + posn.y));
+				}
 			}
-		}
-		else if(board->get_piece(Coord(possible_move[2].x + posn.x, possible_move[2].y + posn.y))!=nullptr){
-			if(board->get_piece(Coord(possible_move[2].x + posn.x, possible_move[2].y + posn.y))->is_black() != this->black && (is_pinned()==-1 || is_pinned()==3)){
-				valid.push_back(Coord(possible_move[2].x + posn.x, possible_move[2].y + posn.y));
+			else if(i==2 && board->get_piece(Coord(possible_move[2].x + posn.x, possible_move[2].y + posn.y))!=nullptr){
+				if(board->get_piece(Coord(possible_move[2].x + posn.x, possible_move[2].y + posn.y))->is_black() != this->black && (is_pinned()==-1 || is_pinned()==3)){
+					valid.push_back(Coord(possible_move[2].x + posn.x, possible_move[2].y + posn.y));
+				}
 			}
-		}
 
-		//Check to see if an en passant move is possible
-		else if(board->get_piece(Coord(posn.x+1, posn.y))->get_name_value()==1 && (is_pinned()==-1 || is_pinned()==1)){
-			if(board->get_piece(Coord(posn.x+1, posn.y))->get_en_passant()){
-				valid.push_back(Coord(possible_move[1].x + posn.x, possible_move[1].y + posn.y));
+			//Check to see if an en passant move is possible
+			else if(i==1 && board->get_piece(Coord(posn.x+1, posn.y))->get_name_value()==1 && (is_pinned()==-1 || is_pinned()==1)){
+				if(board->get_piece(Coord(posn.x+1, posn.y))->get_en_passant()){
+					valid.push_back(Coord(possible_move[1].x + posn.x, possible_move[1].y + posn.y));
+				}
 			}
-		}
-		else if(board->get_piece(Coord(posn.x-1, posn.y))->get_name_value()==1 && (is_pinned()==-1 || is_pinned()==3)){
-			if(board->get_piece(Coord(posn.x-1, posn.y))->get_en_passant()){
-				valid.push_back(Coord(possible_move[2].x + posn.x, possible_move[2].y + posn.y));
+			else if(i==2 && board->get_piece(Coord(posn.x-1, posn.y))->get_name_value()==1 && (is_pinned()==-1 || is_pinned()==3)){
+				if(board->get_piece(Coord(posn.x-1, posn.y))->get_en_passant()){
+					valid.push_back(Coord(possible_move[2].x + posn.x, possible_move[2].y + posn.y));
+				}
 			}
-		}
 
-		//If it is the pawns first move
-		else if(first_move == true && board->get_piece(Coord(posn.x, 1 + posn.y))==nullptr 
-			&& board->get_piece(Coord(posn.x, 2 + posn.y))==nullptr  
-			&& (is_pinned()==-1 || is_pinned()==0)){
-			valid.push_back(Coord(possible_move[3].x + posn.x, possible_move[3].y + posn.y));
+			//If it is the pawns first move
+			else if(i==3 && first_move == true && board->get_piece(Coord(posn.x, 1 + posn.y))==nullptr 
+				&& board->get_piece(Coord(posn.x, 2 + posn.y))==nullptr  
+				&& (is_pinned()==-1 || is_pinned()==0)){
+				valid.push_back(Coord(possible_move[3].x + posn.x, possible_move[3].y + posn.y));
+			}
 		}
 	}
 }
