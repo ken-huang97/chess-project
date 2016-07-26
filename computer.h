3a002e30200a68a36c613a0f108f26c52447e9f4
hakhawaj <hakhawaj@uwaterloo.ca>
2016-07-25 20:43:41 -0400

    Update computer.h

--- computer.h
+++ computer.h
@@ -8,14 +8,4 @@ class Computer:: public Controller{
 	bool is_capture_move(Moving move);
 public:
 	void move() override;
-}#ifndef __COMPUTER_H_
-#define __COMPUTER_H_
-
-class Moving;
-
-class Computer:: public Controller{
-	int level;
-	bool is_capture_move(Moving move);
-public:
-	void move() override;
 }
