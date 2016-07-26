8f8fbb05208147ddbb64f908fb735fb91904eb2d
ken-huang97 <ktkhuang@hotmail.com>
2016-07-25 20:09:34 -0400

    Create computer.h

---
+++ computer.h
@@ -0,0 +1,21 @@
+#ifndef __COMPUTER_H_
+#define __COMPUTER_H_
+
+class Moving;
+
+class Computer:: public Controller{
+	int level;
+	bool is_capture_move(Moving move);
+public:
+	void move() override;
+}#ifndef __COMPUTER_H_
+#define __COMPUTER_H_
+
+class Moving;
+
+class Computer:: public Controller{
+	int level;
+	bool is_capture_move(Moving move);
+public:
+	void move() override;
+}
